e4d42aed1bbae27c53f308fa5b44aab4e9cb6798
Jalal El Hazzat <jalal@anqatech.com>
2025-08-11 14:28:13 +0100

    Added input sanity checks

--- src/modrobot/representation.py
+++ src/modrobot/representation.py
@@ -1,7 +1,18 @@
 import numpy as np
 
 class ModRobot:
-    def __init__(self, rotation_matrix, origin_position):
+    def __init__(self, rotation_matrix, origin_position, check=True):
+        if rotation_matrix.shape != (3, 3):
+            raise ValueError("The rotation matrix must be of dimension 3x3.")
+        if origin_position.shape != (3, 1):
+            raise ValueError("The position vector must be of dimension 3x1.")
+        
+        if check:
+            if not np.allclose(rotation_matrix.T @ rotation_matrix, np.eye(3), atol=1e-8):
+                raise ValueError("The rotation matrix must be orthonormal.")
+            if np.linalg.det(rotation_matrix) < 0.0:
+                raise ValueError("The rotation matrix must have determinant equal to +1.")
+        
         self.rotation_matrix = rotation_matrix
         self.origin_position = origin_position
 
