ade618b16bcee8bc57f2ea68c5fd1015f504e110
Jalal El Hazzat <jalal@anqatech.com>
2025-08-11 15:30:31 +0100

    Added managed attributes methods and improved memory efficiency using __slots__

--- src/modrobot/representation.py
+++ src/modrobot/representation.py
@@ -1,6 +1,13 @@
 import numpy as np
 
 class ModRobot:
+    __slots__ = (
+        "_rotation_matrix",
+        "_origin_position",
+        "_transformation_matrix",
+        "_transformation_matrix_inverse",
+    )
+    
     def __init__(self, rotation_matrix, origin_position, check=True):
         if rotation_matrix.shape != (3, 3):
             raise ValueError("The rotation matrix must be of dimension 3x3.")
@@ -13,20 +20,36 @@ class ModRobot:
             if np.linalg.det(rotation_matrix) < 0.0:
                 raise ValueError("The rotation matrix must have determinant equal to +1.")
         
-        self.rotation_matrix = rotation_matrix
-        self.origin_position = origin_position
+        self._rotation_matrix = rotation_matrix
+        self._origin_position = origin_position
 
-        self.transformation_matrix = self.build_transformation_matrix()
-        self.transformation_matrix_inverse = self.build_transformation_matrix_inverse()
+        self._transformation_matrix = self.build_transformation_matrix()
+        self._transformation_matrix_inverse = self.build_transformation_matrix_inverse()
 
+    @property
+    def rotation_matrix(self):
+        return self._rotation_matrix
+
+    @property
+    def origin_position(self):
+        return self._origin_position
+
+    @property
+    def transformation_matrix(self):
+        return self._transformation_matrix
+    
+    @property
+    def transformation_matrix_inverse(self):
+        return self._transformation_matrix_inverse
+    
     def build_transformation_matrix(self):
         return np.block([
-            [self.rotation_matrix, self.origin_position],
+            [self._rotation_matrix, self._origin_position],
             [ np.zeros( (1, 3) ) ,          1          ]
         ])
-    
+
     def build_transformation_matrix_inverse(self):
         return np.block([
-            [self.rotation_matrix.T, -self.rotation_matrix.T @ self.origin_position],
+            [self._rotation_matrix.T, -self._rotation_matrix.T @ self._origin_position],
             [ np.zeros( (1, 3) ) ,                          1                      ]
         ])
