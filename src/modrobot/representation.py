7399f142877ecda286b5543cad12403400c33821
Jalal El Hazzat <jalal@anqatech.com>
2025-08-11 13:37:47 +0100

    Added the transformation matrix method

--- src/modrobot/representation.py
+++ src/modrobot/representation.py
@@ -6,9 +6,16 @@ class ModRobot:
         self.origin_position = origin_position
 
         self.transformation_matrix = self.build_transformation_matrix()
+        self.transformation_matrix_inverse = self.build_transformation_matrix_inverse()
 
     def build_transformation_matrix(self):
         return np.block([
             [self.rotation_matrix, self.origin_position],
             [ np.zeros( (1, 3) ) ,          1          ]
+        ])
+    
+    def build_transformation_matrix_inverse(self):
+        return np.block([
+            [self.rotation_matrix.T, -self.rotation_matrix.T @ self.origin_position],
+            [ np.zeros( (1, 3) ) ,                          1                      ]
         ])
